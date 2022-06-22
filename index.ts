70af54b46230d915ab50a87c95c4540f851b8fbd
Dzmitry Iliukovich <mr.ilyukovich@gmail.com>
2022-06-22 12:16:19 +0300

    feat: added navigation

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { WebSocketServer } from 'ws';
 import { EOL } from 'os';
 
 import { httpServer } from './src/http_server/index';
+import { Navigate } from './src/robot';
 
 const HTTP_PORT = 3000;
 
@@ -13,6 +14,9 @@ const wss = new WebSocketServer({ port: 8080 });
 wss.on('connection', (ws) => {
   process.stdout.write(`Connection accepted ${EOL}`);
   ws.on('message', (data) => {
-    ws.send(data.toString());
+    const navigate = new Navigate(data);
+    navigate.init();
+    const { x, y } = navigate.getPosition();
+    ws.send(`${data} ${x},${y}`);
   });
 });
